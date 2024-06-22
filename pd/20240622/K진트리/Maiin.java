ab97669e4d16e8f6e9e615114ab86c121a2dccc9
김지희 Kim jihee <50134759+mii2026@users.noreply.github.com>
2024-06-23 02:15:53 +0900

    Create Maiin.java

---
+++ pd/20240622/K진트리/Maiin.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public void solution() throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        long n = Long.parseLong(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
+
+        for(int i = 0; i < q; i++){
+            st = new StringTokenizer(br.readLine());
+            long x = Long.parseLong(st.nextToken());
+            long y = Long.parseLong(st.nextToken());
+
+            if(k == 1){
+                long diff = Math.abs(x - y);
+                sb.append(diff).append("\n");
+                continue;
+            }
+
+            long diff = 0;
+            while(x != y){
+                if(x > y)
+                    x = (x - 2) / k + 1;
+                else
+                    y = (y - 2) / k + 1;
+                diff++;
+            }
+            sb.append(diff).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+    
+    public static void main(String[] args) throws Exception{
+        new Main().solution();
+    }
+}
